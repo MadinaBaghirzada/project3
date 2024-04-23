57a1ecc979320cea28b254f7d79cbf6593d72a43
madina <medinebagirzade64@gmail.com>
2024-04-23 19:50:20 +0400

    hjgjhgj

---
+++ Project3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Project3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("maashinizi qeyd edin");
+        int maash = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Istediyiniz bonusu qeyd edin");
+        int bonus = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Pul(maash, bonus));
+    }
+
+    static int Pul(int a, int b)
+    {
+        return a + (a * b / 100);
+    }
+}
+
